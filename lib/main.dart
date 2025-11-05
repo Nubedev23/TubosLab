4fffe10135b279609e0fc6b8d5b364ced3e17be2
Melisa Budini <devmelb23@gmail.com>
2025-11-05 15:36:56 -0300

    fix(main): Ajustes en main.dart y configuracion de gitignore antes de merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'utils/app_styles.dart';
 import 'pantallas/pantalla_bienvenida.dart';
 import 'pantallas/pantalla_principal.dart';
